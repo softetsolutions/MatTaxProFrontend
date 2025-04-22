4a76612302427fc0c57019ebddfe6a85d743d710
st17931 <st17931@gmail.com>
2025-04-23 00:23:10 +0530

    Did some change in the verify email comp

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 function VerifyEmail (){
     const [loading, setLoading] = useState(false);
+    const [error, setError]=useState("");
 
     const param = useParams();
     console.log("param", param);
@@ -10,14 +11,32 @@ function VerifyEmail (){
     useEffect(()=>{
 
         async function fetchData(){
+            try{
+                setLoading(true);
             const res = fetch(`${import.meta.env.VITE_BASE_URL}/verify/${param.token}`);
+            if(res.status !== 200){
+                throw new Error("Got some Error in verifying email! Please try again")
+            }
+            }catch(e){
+                setError(e);
+            }finally{
+                setLoading(false)
+            }
         }
         fetchData()
     },[]);
 
+    if (loading) {
+        return (
+          <div className="flex justify-center items-center h-screen">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+          </div>
+        );
+      }
+
     return(
         <div className="flex min-h-screen bg-white text-gray-900">
-           {loading ?  <div>Verifying Email</div> : <div>Email Verified</div>}
+           {!error ? <div>Email Verified</div> : <div>{error}</div>}
         </div>
     )
 
