89528edf0589e1b2a5f510a435fef5322c23ace2
Harshjosh361 <harshjosh361@gmail.com>
2025-05-29 07:22:17 +0530

    feat: update loading spinner color to blue in Reports component

--- src/pages/user/Reports.jsx
+++ src/pages/user/Reports.jsx
@@ -76,7 +76,7 @@ const filteredTransactions = allTransaction.filter((txn) => {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
       </div>
     );
   }
